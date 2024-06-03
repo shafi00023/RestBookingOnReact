59efd2f5d81197a20573f6361c7b9b2df2dcad2a
Aesha Darji <Aesha.Darji@theaccessgroup.com>
2024-06-03 18:25:11 +0530

    added login and signup component

---
+++ src/components/Signup/signup.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../Login/login.css";
+
+const Signup: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [contactNo, setContactNo] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/signup", { email, password });
+      navigate("/login");
+    } catch (error) {
+      console.error("Signup error", error);
+    }
+  };
+
+  const handleLoginRedirect = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2>Sign Up</h2>
+        <form className="login-form" onSubmit={handleSignup}>
+          <div className="login-devision">
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="login-devision">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="login-devision">
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="login-devision">
+            <label>Contact No:</label>
+            <input
+              type="tel"
+              value={contactNo}
+              onChange={(e) => setContactNo(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Create Account</button>
+          <button onClick={handleLoginRedirect} className="signup-button">
+            alredy have account ? Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
