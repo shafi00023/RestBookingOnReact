597923544f8d73d2d0d25dcb67dbd63b365e444e
Aesha Darji <Aesha.Darji@theaccessgroup.com>
2024-06-10 17:48:33 +0530

    book-table ui added

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './header.css';
+import React, { useState } from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <p>ReserveEat</p>
-        </header>
-    );
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+  return (
+    <header className="header">
+      <div className="header-main-wrapper">
+        <p>ReserveEat</p>
+        <div className="user-section">
+          <FontAwesomeIcon icon={faCircleUser} color="white" />
+          <span>Adam Smith</span>
+          <FontAwesomeIcon
+            icon={faCaretDown}
+            color="white"
+            onClick={toggleDropdown}
+          />
+          {isDropdownVisible && (
+            <div className="dropdown-menu">
+              <p>Sign out</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
